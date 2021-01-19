275b02b15d54b5c82c6c730ce0e5826778193b83
jinsujeong <ejd090393@gmail.com>
2021-01-19 10:53:22 +0900

    1주차 파일 업로드

--- day3/forLab3.py
+++ day3/forLab3.py
@@ -3,7 +3,7 @@ evensum = 0
 x = random.randint(1, 10)
 y = random.randint(30, 40)
 
-for d in (x,y):   # range(x, y+1)
+for d in range(x, y+1):
     if d % 2 == 0:
         evensum += d
 print(x,"부터",y, "까지의 짝수의 합 :",evensum)
